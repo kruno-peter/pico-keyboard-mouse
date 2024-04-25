87f5fa7bd1654f72981decadd7a7efbae8bd847f
KP <88709841+kruno-peter@users.noreply.github.com>
2024-04-25 12:58:04 +0200

    Add files via upload

---
+++ typing_string.py
@@ -0,0 +1,10 @@
+# emulating a keyboard: typing 'n42'
+import usb_hid
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keycode import Keycode
+from time import sleep
+
+keyboard = Keyboard(usb_hid.devices)
+
+sleep(4.2)
+keyboard.send(Keycode.N, Keycode.FOUR, Keycode.TWO)
